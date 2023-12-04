a0429d22be640899a77ebb6085363db52a9f8a61
EC2 Default User <piyopiyoblue5@gmail.com>
2023-12-04 04:22:49 +0000

    雛形作成

---
+++ db/migrate/20231204033526_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.text :post
+      t.integer :post_status
+      t.text :address
+      # MySQLに切り替えるタイミングでデータ型をdoubleに変更
+      t.float :latitude
+      t.float:longitude
+
+      t.timestamps
+    end
+  end
+end

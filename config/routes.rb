675e8d4f03337273f4de577e6f74409b05d01293
EC2 Default User <piyopiyoblue5@gmail.com>
2023-12-06 02:06:17 +0000

    不足コントローラー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'notifications/index'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
   devise_for :admins, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
